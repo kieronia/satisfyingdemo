2b7f8d1d45297c3a6e0cc22e273323b0ca811e49
Kieronia <70661699+kieronia@users.noreply.github.com>
2020-09-03 16:45:50 +0100

    Add files via upload

---
+++ epik.py
@@ -0,0 +1,25 @@
+import os
+from random import *
+import time
+size = int(15)
+decrease = "no"
+
+
+
+
+
+while True:
+
+	cmd = (f'mode {size},{size}')
+	os.system(cmd)
+	#time.sleep(0.01)
+	size = size + 1
+	os.system("color 9f")
+
+	if size > 100:
+		for kieronia in range(0,85):
+			size = size - 1
+			os.system("color bf")
+			cmd = (f'mode {size},{size}')
+			os.system(cmd)
+			#time.sleep(0.01)
